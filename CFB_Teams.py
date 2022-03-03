98bc5df803a760069d63bafa22c6be4c9591e055
m00nscar <64382520+m00nscar@users.noreply.github.com>
2022-03-03 10:42:02 -0500

    Update CFB_Teams.py

--- CFB_Teams.py
+++ CFB_Teams.py
@@ -47,44 +47,46 @@ engine = postgres_database_connection()
 api_instance = cfbd.TeamsApi(cfbd.ApiClient(configuration))
 conference = 'ACC' # str | Conference abbreviation filter (optional)
 api_response = api_instance.get_teams()
-#print(api_response)
-
-#### CONVERT API RESPONSE TO PANDAS DATAFRAME ###
-df = pd.DataFrame.from_records([p.to_dict() for p in api_response])
-### BREAK OUT THE NESTED VALUES FROM API RESPONSE ###
-df['city'] = df[['location']].apply(lambda x: x.location['city'], axis=1)
-df['capacity'] = df[['location']].apply(lambda x: x.location['capacity'], axis=1)
-df['dome'] = df[['location']].apply(lambda x: x.location['dome'], axis=1)
-df['elevation'] = df[['location']].apply(lambda x: x.location['elevation'], axis=1)
-df['grass'] = df[['location']].apply(lambda x: x.location['grass'], axis=1)
-df['latitude'] = df[['location']].apply(lambda x: x.location['latitude'], axis=1)
-df['longitude'] = df[['location']].apply(lambda x: x.location['longitude'], axis=1)
-df['location_name'] = df[['location']].apply(lambda x: x.location['name'], axis=1)
-df['state'] = df[['location']].apply(lambda x: x.location['state'], axis=1)
-df['timezone'] = df[['location']].apply(lambda x: x.location['timezone'], axis=1)
-df['venue_id'] = df[['location']].apply(lambda x: x.location['venue_id'], axis=1)
-df['year_constructed'] = df[['location']].apply(lambda x: x.location['year_constructed'], axis=1)
-df.head()
-#print(df)
-
-#print(df.to_json())
-
-## CONVERT EACH ROW OF DATAFRAM BACK TO JSON ####
-df = df.apply(lambda x: x.to_json(), axis=1)
-#print(df)
 
 
-
+#
+##### CONVERT API RESPONSE TO PANDAS DATAFRAME ###
+#df = pd.DataFrame.from_records([p.to_dict() for p in api_response])
+#### BREAK OUT THE NESTED VALUES FROM API RESPONSE ###
+#df['city'] = df[['location']].apply(lambda x: x.location['city'], axis=1)
+#df['capacity'] = df[['location']].apply(lambda x: x.location['capacity'], axis=1)
+#df['dome'] = df[['location']].apply(lambda x: x.location['dome'], axis=1)
+#df['elevation'] = df[['location']].apply(lambda x: x.location['elevation'], axis=1)
+#df['grass'] = df[['location']].apply(lambda x: x.location['grass'], axis=1)
+#df['latitude'] = df[['location']].apply(lambda x: x.location['latitude'], axis=1)
+#df['longitude'] = df[['location']].apply(lambda x: x.location['longitude'], axis=1)
+#df['location_name'] = df[['location']].apply(lambda x: x.location['name'], axis=1)
+#df['state'] = df[['location']].apply(lambda x: x.location['state'], axis=1)
+#df['timezone'] = df[['location']].apply(lambda x: x.location['timezone'], axis=1)
+#df['venue_id'] = df[['location']].apply(lambda x: x.location['venue_id'], axis=1)
+#df['year_constructed'] = df[['location']].apply(lambda x: x.location['year_constructed'], axis=1)
+#df.head()
+##print(df)
+#
+##print(df.to_json())
+#
+### CONVERT EACH ROW OF DATAFRAM BACK TO JSON ####
+#df = df.apply(lambda x: x.to_json(), axis=1)
+##print(df)
+#
+#
+#
 ### INITIALIZE CURSOR ##
 connection = postgres_database_connection()
 cursor = connection.connect()
 
 # INSERT EACH JSON ROW TO DATABASE TABLE ###
-for i in df:  
-
+for i in api_response:  
+    i = i.to_dict()
+    i = json.dumps(i, indent = 4) 
     i = str(i).replace("'", '')
-    insert_sql = "INSERT INTO cfb.json_teams (json_raw) VALUES ('{insert}')".format(insert=i)
-    #sqlalchemy.text(insert_sql)
+    insert_sql = "INSERT INTO cfb.test (json_raw) VALUES ('{insert}')".format(insert=i)
+   # sqlalchemy.text(insert_sql)
     cursor.execute(insert_sql)
     print(" ")
     print(insert_sql)
