55092a79d8a3b74afeb926c42e53c033e57af743
m00nscar <64382520+m00nscar@users.noreply.github.com>
2022-03-04 15:33:33 -0500

    commit

--- CFB_Plays.py
+++ CFB_Plays.py
@@ -25,7 +25,7 @@ def postgres_database_connection():
     """Returns the DB engine"""
     try:
         print('Connecting to DB')
-        conn =  "postgresql+psycopg2://dba_richie:changeme@73.147.167.79:5432/postgres" 
+        conn =  "postgresql+psycopg2://dba_richie:changeme@73.147.167.79:5432/rw_cfb" 
         engine = create_engine(conn)
         print('Connected to DB')
         return engine
